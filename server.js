a4f13e1f9e8e7176844079789d8ee78b9107d848
Filip FICO <fitz993m@gmail.com>
2019-02-12 23:04:47 +0100

    add public css, changed error in handlebars and 404 page

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
+const path = require("path");
 
 const posts = require("./routes/posts");
 const users = require("./routes/users");
+const rest = require("./routes/rest");
 
 mongoose
   .connect("mongodb://localhost/blog-test", { useNewUrlParser: true })
@@ -15,11 +17,17 @@ mongoose
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view in engine", "handlebars");
+app.set("view engine", "handlebars");
 
 app.use("/posts", posts);
 app.use("/users", users);
+app.use("/", rest);
 
+app.use((req, res) => {
+  res.render("rest/404");
+});
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening to port ${port}`));
